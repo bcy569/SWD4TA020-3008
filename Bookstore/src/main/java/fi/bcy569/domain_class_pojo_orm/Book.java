5686843832c92df0975913a5de316a8a8d19cd7e
bcy569 <bcy569@myy.haaga-helia.fi>
2021-09-10 09:10:02 +0300

    Clean (lombok)

--- Bookstore/src/main/java/fi/bcy569/domain_class_pojo_orm/Book.java
+++ Bookstore/src/main/java/fi/bcy569/domain_class_pojo_orm/Book.java
@@ -21,35 +21,35 @@ public class Book {
 	private int year, price;
 
 	
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public String getIsbn() {
-		return isbn;
-	}
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-	public int getYear() {
-		return year;
-	}
-	public void setYear(int year) {
-		this.year = year;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
+//	public String getTitle() {
+//		return title;
+//	}
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//	public String getAuthor() {
+//		return author;
+//	}
+//	public void setAuthor(String author) {
+//		this.author = author;
+//	}
+//	public String getIsbn() {
+//		return isbn;
+//	}
+//	public void setIsbn(String isbn) {
+//		this.isbn = isbn;
+//	}
+//	public int getYear() {
+//		return year;
+//	}
+//	public void setYear(int year) {
+//		this.year = year;
+//	}
+//	public int getPrice() {
+//		return price;
+//	}
+//	public void setPrice(int price) {
+//		this.price = price;
+//	}
 
 }
