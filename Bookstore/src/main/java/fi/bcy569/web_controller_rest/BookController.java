c0a2463c45f6e8dfea25cbcc4b9c29ad5de8bc5c
bcy569 <bcy569@myy.haaga-helia.fi>
2021-09-10 12:03:14 +0300

    Chapter 3 Edit Bookstore

--- Bookstore/src/main/java/fi/bcy569/web_controller_rest/BookController.java
+++ Bookstore/src/main/java/fi/bcy569/web_controller_rest/BookController.java
@@ -3,7 +3,6 @@ package fi.bcy569.web_controller_rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,54 +17,58 @@ public class BookController {
 
 	@Autowired
 	private IBookRepository bookRepository;
-	
-	
-	@RequestMapping("booklist")
-	public String getBookList(Model model){
-		
-		model.addAttribute("books", bookRepository.findAll());
-		
-		return "booklist";
-		
-	}
 
+	@RequestMapping("booklist")
+	public String getBookList(Model model) {
+
+		model.addAttribute("books", bookRepository.findAll());
+
+		return "booklist";
+
+	}
 
 	@GetMapping("addbook")
 	public String getAddBook(Model model) {
-		
+
 		model.addAttribute("book", new Book());
-		
+
 		return "addbook";
-		
+
 	}
 
-	
 	@PostMapping("add")
-	public String setAdd(@ModelAttribute Book book,
-			Model model) {
+	public String setAdd(@ModelAttribute Book book) {
 
 		bookRepository.save(book);
-		
+
 		return "redirect:/booklist";
-		
+
 	}
-	
+
+	@PostMapping("update")
+	public String putUpdate(@ModelAttribute Book book) {
+
+		bookRepository.save(book);
+
+		return "redirect:/booklist";
+
+	}
+
 	@GetMapping("delete/{id}")
 	public String deleteBookById(@PathVariable("id") Long id) {
 
 		bookRepository.deleteById(id);
-		
+
 		return "redirect:/booklist";
-		
+
+	}
+
+	@GetMapping("edit/{id}")
+	public String editBookById(@PathVariable("id") Long id, Model model) {
+
+		model.addAttribute("book", bookRepository.findById(id));
+		return "editbook";
+
 	}
 
 }
-
-
-//public String getBookList(@RequestParam(name="title", required=false) String title,
-//		@RequestParam(name="auihor", required=false) String auihor,
-//		@RequestParam(name="year", required=false) String year,
-//		@RequestParam(name="isbn", required=false) String isbn,
-//		@RequestParam(name="price", required=false) String price,
-//		@ModelAttribute Model model)
-//{
