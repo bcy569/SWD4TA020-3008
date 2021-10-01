82a37668c819952a3ba35e2a19e1f3a571c187ce
bcy569 <bcy569@myy.haaga-helia.fi>
2021-10-01 18:59:42 +0300

    Remove

--- backend-fullstack-skeleton-java-spring-maven/src/main/java/fi/dev/academy/vaccinationdatabase/domain_class_pojo_orm/order/OrderNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package fi.dev.academy.vaccinationdatabase.domain_class_pojo_orm.order;
-
-public class OrderNotFoundException extends RuntimeException  {
-
-    public OrderNotFoundException(Long id) {
-        super("Could not find order " + id);
-    }
-}
